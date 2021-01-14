2790894cf431bc6017b60d81b7b323243057b2d6
Bluette1 <marylene.sawyer@gmail.com>
2021-01-14 14:31:57 +0200

    Add more tests - `MealList` component

--- src/_tests_/App.test.js
+++ src/_tests_/App.test.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { render, screen, cleanup, fireEvent, act, waitFor } from '@testing-library/react';
+import {
+  render, screen, cleanup, fireEvent, act, waitFor,
+} from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import axios from 'axios';
 import App from '../components/App';
 import configureStore, { history } from '../store';
-import axios from 'axios';
 
 configure({ adapter: new Adapter() });
 
@@ -24,14 +26,14 @@ const AppWithStore = () => (
   </Provider>
 );
 test('renders the app', async () => {
-  axios.get.mockImplementation((url) => {
+  axios.get.mockImplementation(url => {
     switch (url) {
       case 'https://www.themealdb.com/api/json/v1/1/categories.php':
-        return Promise.resolve({data: {categories: []}});
+        return Promise.resolve({ data: { categories: [] } });
       default:
-        return Promise.resolve({data: {meals: []}});
+        return Promise.resolve({ data: { meals: [] } });
     }
-  })
+  });
   render(<AppWithStore />);
   await waitFor(() => {
     const catalogueTitle = screen.getByText(/Recipe Catalogue/i);
@@ -40,14 +42,14 @@ test('renders the app', async () => {
 });
 
 it('renders app with home button component', async () => {
-  axios.get.mockImplementation((url) => {
+  axios.get.mockImplementation(url => {
     switch (url) {
       case 'https://www.themealdb.com/api/json/v1/1/categories.php':
-        return Promise.resolve({data: {categories: []}});
+        return Promise.resolve({ data: { categories: [] } });
       default:
-        return Promise.resolve({data: {meals: []}});
+        return Promise.resolve({ data: { meals: [] } });
     }
-  })
+  });
   const div = document.createElement('div');
   const rendered = render(<AppWithStore />, div);
   await waitFor(() => {
@@ -62,12 +64,12 @@ it('renders the app with expected component -test with enzyme', async () => {
   axios.get.mockResolvedValue({
     data: { meals: [] },
   });
-  axios.get.mockImplementation((url) => {
+  axios.get.mockImplementation(url => {
     switch (url) {
       case 'https://www.themealdb.com/api/json/v1/1/categories.php':
-        return Promise.resolve({data: {categories: []}});
+        return Promise.resolve({ data: { categories: [] } });
       default:
-        return Promise.resolve({data: {meals: []}});
+        return Promise.resolve({ data: { meals: [] } });
     }
   });
   let app;
--- src/_tests_/MealList.test.js
+++ src/_tests_/MealList.test.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { render, screen, cleanup, fireEvent, waitFor } from '@testing-library/react';
+import {
+  render, screen, cleanup, fireEvent, waitFor,
+} from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
+import axios from 'axios';
 import MealsList from '../containers/MealsList';
 import configureStore, { history } from '../store';
-import axios from 'axios';
 
 jest.mock('axios');
 
@@ -19,9 +21,7 @@ const MealsListWithStore = () => (
     </React.StrictMode>
   </Provider>
 );
-
-test('', ()=>{});
-it('renders app with MealList component', async () => {
+it('renders the MealList component', async () => {
   const meals = [{
     idMeal: '1',
     strMeal: 'Mealia',
@@ -39,11 +39,85 @@ it('renders app with MealList component', async () => {
   });
   const rendered = render(<MealsListWithStore />);
   await waitFor(() => {
-  const mealTitle = rendered.getByText('Mealia');
-  expect(mealTitle).toBeInTheDocument();
+    const mealTitle = rendered.getByText('Mealia');
+    expect(mealTitle).toBeInTheDocument();
 
-  const mealCategory = rendered.getByText('Dessert');
-  expect(mealCategory).toBeInTheDocument();
-  expect(rendered).toMatchSnapshot();
-  })
+    const mealCategory = rendered.getByText('Dessert');
+    expect(mealCategory).toBeInTheDocument();
+
+    const streetArea = rendered.getByText('British');
+    expect(streetArea).toBeInTheDocument();
+
+    const tags = rendered.getByText('Tags: dessert, British');
+    expect(tags).toBeInTheDocument();
+    expect(rendered).toMatchSnapshot();
+  });
+});
+
+test('renders MealList component with the expected button elements', async () => {
+  const meals = [{
+    idMeal: '1',
+    strMeal: 'Mealia',
+    strCategory: 'Dessert',
+    strMealThumb: 'image-url',
+    strTags: 'dessert, British',
+    strArea: 'British',
+    strYoutube: 'Youtube url',
+    strInstructions: 'How to Cook Mealia',
+    strIngredient1: 'corn',
+    strMeasure1: '1 cup',
+  }];
+  axios.get.mockResolvedValue({
+    data: { meals },
+  });
+  const rendered = render(<MealsListWithStore />);
+  await waitFor(() => {
+    const hideButton = rendered.getByText('Hide from list');
+    fireEvent.click(hideButton);
+    expect(hideButton).toBeDefined();
+
+    const detailsButton = rendered.getByText('View details');
+    fireEvent.click(detailsButton);
+    expect(detailsButton).toBeDefined();
+    expect(screen.findAllByRole('button')).toBeDefined();
+
+    expect(rendered).toMatchSnapshot();
+  });
+});
+
+test('renders MealList component with the `hideMeal` button working correctly', async () => {
+  const meals = [{
+    idMeal: '1',
+    strMeal: 'Mealia',
+    strCategory: 'Dessert',
+    strMealThumb: 'image-url',
+    strTags: 'dessert, British',
+    strArea: 'British',
+    strYoutube: 'Youtube url',
+    strInstructions: 'How to Cook Mealia',
+    strIngredient1: 'corn',
+    strMeasure1: '1 cup',
+  }];
+  axios.get.mockResolvedValue({
+    data: { meals },
+  });
+  const rendered = render(<MealsListWithStore />);
+  await waitFor(() => {
+    const mealTitle = rendered.getByText('Mealia');
+
+    const mealCategory = rendered.getByText('Dessert');
+
+    const streetArea = rendered.getByText('British');
+
+    const tags = rendered.getByText('Tags: dessert, British');
+    const hideButton = rendered.getByText('Hide from list');
+    fireEvent.click(hideButton);
+    expect(hideButton).toBeDefined();
+    expect(mealTitle).not.toBeInTheDocument();
+    expect(mealCategory).not.toBeInTheDocument();
+    expect(streetArea).not.toBeInTheDocument();
+    expect(tags).not.toBeInTheDocument();
+
+    expect(rendered).toMatchSnapshot();
+  });
 });
