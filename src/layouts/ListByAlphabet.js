cab50530601680d21e037710de78878c90306567
Bluette1 <marylene.sawyer@gmail.com>
2021-01-15 15:13:33 +0200

    Modify the home title

--- src/layouts/ListByAlphabet.js
+++ src/layouts/ListByAlphabet.js
@@ -4,7 +4,7 @@ import '../css/ListByAlphabet.css';
 
 const ListByAlphapet = () => (
   <div className="list-by-alphabet">
-    <h2>Browse our List of Recipes </h2>
+    <h2>Browse available Recipes </h2>
     <div className="ul">
       <li><a href="/search?f=a">A</a></li>
       <li><a href="/search?f=b">B</a></li>
